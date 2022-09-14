8ebc24c50c827d5bf9c09f6218078e209b364b2a
Olabamiji Yusuf <yfact247@gmail.com>
2022-09-14 03:29:30 +0000

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - Prints character
+ *
+ * Return: Always char c on (Success)
+ */
+int _putchar(char c)
+{
+	return (char) c;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+/*
+ * header: my first header
+ *
+ * Description: This header is linked in other files
+ */
+int _putchar(char c);
