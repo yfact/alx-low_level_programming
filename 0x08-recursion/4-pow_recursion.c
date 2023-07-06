018fdbcd012a15c5b2d641390dc9ac950798f42c
Olabamiji Yusuf <yfact247@gmail.com>
2023-07-06 14:12:14 +0000

    add assignments

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise x to power y
+ * @x: base
+ * @y: power
+ *
+ * Return: on success 1
+ * on error, -1 is returned
+ */
+int _pow_recursion(int x, int y)
+{
+	if (n < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,3 +2,7 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int sqrt2(int a, int b);
+int _sqrt_recursion(int n);
+int prime2(int a, int b);
+int is_prime_number(int n);
