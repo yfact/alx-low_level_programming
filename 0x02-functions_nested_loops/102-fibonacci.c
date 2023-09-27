3d9b991399e212d2af80101667ea311421730c1d
Olabamiji Yusuf <yfact247@gmail.com>
2023-09-27 13:38:51 +0000

    add extra function files

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i, n, m, c, u;
+
+	n = 1;
+	m = 2;
+	u = 48;
+	printf("%li, ", n);
+	printf("%li, ", m);
+	for (i = 1; i <= u; i++)
+	{
+	c = m + n;
+	n = m;
+	m = c;
+	if (i != u)
+	{
+	printf("%li, ", c);
+	}
+	else
+	{
+	printf("%li\n", c);
+	}
+	}
+	return (0);
+}
