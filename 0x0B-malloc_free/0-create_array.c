2070555861a3b49509688ab263cabecb2601bb6c
olabamiji yusuf <yfact247@gmail.com>
2023-11-22 03:15:46 -0800

    Write a program that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,12 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	if (size <= 0)
-		return 	NULL;
+		return (NULL);
 
 	s = malloc(sizeof(char) * size);
 
 	if (s == 0)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		*(s + i) = c;
